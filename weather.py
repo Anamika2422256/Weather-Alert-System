780750b8dc6ed2a84849ffb363204c8ce5dbccd4
Anamika Ravi <anamikaravi442@gmail.com>
2026-06-13 16:28:31 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,14 +1,15 @@
 import requests
 import smtplib
 from email.message import EmailMessage
-import os
 
 API_KEY = "your_openweathermap_api_key"
 CITY = "Kochi"
+
 EMAIL = "your_email@gmail.com"
 APP_PASSWORD = "your_app_password"
 
-receiver = os.environ.get("RECEIVER_EMAIL")
+receiver = "receiver@gmail.com"
+
 url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
 
 response = requests.get(url)
@@ -26,25 +27,26 @@ if data.get("cod") == 200:
 
     if temperature > 35 or condition.lower() == "rain":
 
-    msg = EmailMessage()
+        msg = EmailMessage()
 
-    msg["Subject"] = "Weather Alert"
-    msg["From"] = EMAIL
-    msg["To"] = receiver
+        msg["Subject"] = "Weather Alert"
+        msg["From"] = EMAIL
+        msg["To"] = receiver
 
-    msg.set_content(
-        f"Weather Alert!\n\n"
-        f"Temperature: {temperature}°C\n"
-        f"Condition: {condition}"
-    )
+        msg.set_content(
+            f"Weather Alert!\n\n"
+            f"Temperature: {temperature}°C\n"
+            f"Condition: {condition}"
+        )
 
-      with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login(EMAIL, APP_PASSWORD)
-        smtp.send_message(msg)
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+            smtp.login(EMAIL, APP_PASSWORD)
+            smtp.send_message(msg)
 
         print("Weather alert email sent!")
 
     else:
-          print("No alert needed.")
+        print("No alert needed.")
+
 else:
     print("Weather API error:", data)
